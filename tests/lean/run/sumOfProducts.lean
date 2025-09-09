a7e3769daa3d5b4612b09228c346678af94f9e85
Wojciech Rozowski <wojciech@lean-fro.org>
2025-09-09 15:02:22 +0100

    works!

--- tests/lean/run/sumOfProducts.lean
+++ tests/lean/run/sumOfProducts.lean
@@ -1,14 +1,10 @@
-set_option trace.Meta.SumOfProducts true
 
-def test (a b : Prop) := (fun x : a => b)
-
-#check test True True
-
-theorem myExists (a b : Prop) : ∃ (_ : a), b ↔ a ∧ b := by sorry
 
 inductive my_prop (r : α → α → Prop) : α → Prop where
-  | mk2 : my_prop r a → my_prop r a
+  | mk2 : my_prop r a → r a b → my_prop r b
 
 #check my_prop.casesOn
-#print my_prop.sop
+
+#check my_prop.sop
+
 #check Exists
