3b95089c41b3e063e0fbcc78d0cdc826f0582b19
Wojciech Rozowski <wojciech@lean-fro.org>
2026-03-18 17:34:23 +0000

    chore: add example

--- tests/elab/cbv_string_split.lean
+++ tests/elab/cbv_string_split.lean
@@ -1,11 +1,28 @@
+import Std
+
 example : ("hello world".split (· == ' ')).toList.map (·.toString) = ["hello", "world"] := by cbv
 
 example : ("hello world".split (· = ' ')).toList.map (·.toString) = ["hello", "world"] := by cbv
 
 example : ("hello world".split (' ')).toList.map (·.toString) = ["hello", "world"] := by cbv
 
-example : (("hello world".split (· == ' ')).filter (fun x => true )).toList.map (·.toString) = ["hello", "world"] := by cbv
+def numbers : List String :=
+  ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
-example : (("hello world".split (· = ' ')).filter (fun x => true )).toList.map (·.toString) = ["hello", "world"] := by cbv
+def numberToNumber : Std.HashMap String Nat :=
+  (0...=9).iter.fold (init := ∅) (fun sofar num => sofar.insert numbers[num]! num)
 
-example : (("hello world".split (' ')).filter (fun x => true )).toList.map (·.toString) = ["hello", "world"] := by cbv
+def sortNumbers (str : String) : String :=
+  str.split ' '
+    |>.filter (!·.isEmpty)
+    |>.map (numberToNumber[·.copy]!)
+    |>.toList
+    |>.mergeSort
+    |>.map (numbers[·]!)
+    |> String.intercalate " "
+
+example : sortNumbers "" = "" := by cbv
+example : sortNumbers "three" = "three" := by cbv
+example : sortNumbers "three five" = "three five" := by cbv
+example : sortNumbers "five three" = "three five" := by cbv
+example : sortNumbers "one one" = "one one" := by cbv
