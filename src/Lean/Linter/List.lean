ae7d3f8e83f68d337d9798bc75cd80583cd03f30
thorimur <68410468+thorimur@users.noreply.github.com>
2026-03-24 18:07:59 -0400

    chore: rename `Lean.Linter.Basic` -> `Lean.Linter.Init`

--- src/Lean/Linter/List.lean
+++ src/Lean/Linter/List.lean
@@ -8,7 +8,7 @@ module
 prelude
 public import Lean.Elab.Command
 public import Lean.Server.InfoUtils
-import Lean.Linter.Basic
+import Lean.Linter.Init
 
 public section
 set_option linter.missingDocs true -- keep it documented
